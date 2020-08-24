246e2cbac3ea9452b53847eab4c4d5a2e0872f51
jbn0619 <jbn5379@g.rit.edu>
2020-08-24 10:54:36 -0400

    Completed Exercise 1
    Completed and submitted exercise 1 for DSA2.

---
+++ E01 - VS19 Config/Solution/Main.cpp
@@ -0,0 +1,17 @@
+/*--------------------------------------------------------------------------------------------------
+Comments about this program
+--------------------------------------------------------------------------------------------------*/
+#include "Main.h";
+using namespace std;
+
+int main()
+{
+	cout << "Hello world!\n";
+
+	//Ending the program
+	cout << "Press enter to end the program.";
+
+	getchar();
+
+	return 0;
+}
