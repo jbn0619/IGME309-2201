ddf21d5bbe7d39d93c0a46746ce42bafe9675320
jbn0619 <jbn5379@g.rit.edu>
2020-08-29 17:05:42 -0400

    Began E02
    Began work on E02. Currently running into a bug where the program can't access the shaders directory for some reason.

---
+++ E02 - VS19 GLFW Config/E02 - VS19 GLFW Config/E02 - VS19 GLFW Config/Main.h
@@ -0,0 +1,20 @@
+/*-------------------------------------------------------------------------------
+This project was generated in 2020
+-------------------------------------------------------------------------------*/
+#ifndef _MAIN_H
+#define _MAIN_H
+
+#include <iostream>
+
+// Include standard headers
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "GL\glew.h"
+#include "GLFW\glfw3.h"
+
+#include "GLM\glm.hpp"
+
+#include "OpenGL-Tutorials\shader.hpp"
+
+#endif
